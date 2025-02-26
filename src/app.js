e72f69b5b93d1cfc450040234cb968cea986030f
Anunn711 <156364902+Anunn711@users.noreply.github.com>
2025-02-25 19:55:59 -0600

    I've worked on this for a while, Here is a break down of what this commit adds
    Added a styles sheet and comments inside to make it easy for reused
    
    Castle.jpeg has been chosen as the background for the login
    
    app.js was modified to be able to see the css folder under public
    
    Login hbs been altered to the design i laid forth in our talks

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const path = require('path');
 const app = express(); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,16 +30,16 @@ const UserDAO = require('./User/user.dao');
 
 
 const LoginService = require('./Login/login.service');
-const UserService = require('./User/user.service');
+//const UserService = require('./User/user.service');
 
 const userDAO = new UserDAO(prisma);
 const loginService = new LoginService(userDAO);
-const userService = new UserService(userDAO, bcrypt);
+//const userService = new UserService(userDAO, bcrypt);
 
 const loginController = require('./Login/login.controller');
-const UserController = require('./User/user.controller');
+//const UserController = require('./User/user.controller');
 
-app.use(UserController(userService));
+//app.use(UserController(userService));
 app.use(loginController(loginService));
 
 // Error handler
