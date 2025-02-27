601f1c2e989bd9d865a5dcefba68e2d145e3a0ea
Zachary <zwd7@students.uwf.edu>
2025-02-27 12:37:18 -0600

    dashborad view

--- src/Login/login.controller.js
+++ src/Login/login.controller.js
@@ -7,4 +7,7 @@ router.get('/login', (req, res) => {
 router.get('/register', (req,res) =>{
     res.render('register');
 });
+router.get('/dashboard', (req,res) =>{
+    res.render('dashboard');
+});
 module.exports = router;
