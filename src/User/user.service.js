c9284c94d87c26e021bb11ccf501ec34794f239b
Casey Ingram <ci12@students.uwf.edu>
2025-03-03 13:08:42 -0600

    Added test coverages

--- src/User/user.service.js
+++ src/User/user.service.js
@@ -8,6 +8,10 @@ class UserService {
     async registerUser(userData) {
         try {
             console.log('Creating user with data:', userData);
+            const existingUser = await this.userDAO.findUserByEmail(userData.email);
+            if (existingUser) {
+                throw new Error('Email already exists');
+            }
             userData.password = await bcrypt.hash(userData.password, 10);
             return await this.userDAO.createUser(userData);
         } catch (error) {
